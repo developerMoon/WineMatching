3caa9fbf88f719d293d05222d0dad54ccca2382d
Moon <moonsoyoung24@gmail.com>
2018-01-16 16:29:35 +0900

    detail font 변경

--- src/collaboproject2/WineDetail.java
+++ src/collaboproject2/WineDetail.java
@@ -21,6 +21,7 @@ import javax.swing.JButton;
 import javax.swing.JScrollBar;
 import javax.swing.JSlider;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 	//연결해야함
 	//글씨체가 다 너무 작음
@@ -83,50 +84,70 @@ public class WineDetail extends JFrame implements ActionListener{
 		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
 		
 		JLabel lblNewLabel_5 = new JLabel("가격");
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_5.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_5);
 		
 		JLabel price = new JLabel("New label");
 		panel_1.add(price);
+		price.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		price.setText(vo.getPrice()+"");
 		
 		
 		JLabel lblNewLabel_2 = new JLabel("원산지");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_2);
 		
 		JLabel country = new JLabel("New label");
 		panel_1.add(country);
+		country.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		country.setText(vo.getCountry());
 		
 		JLabel lblNewLabel_10 = new JLabel("빈티지");
+		lblNewLabel_10.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_10.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_10);
 		
 		JLabel vintage = new JLabel("New label");
 		panel_1.add(vintage);
+		vintage.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		vintage.setText(vo.getVintage()+"");
 		
 		JLabel lblNewLabel_4 = new JLabel("타입");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_4);
 		
 		JLabel type = new JLabel("New label");
 		panel_1.add(type);
+		type.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		type.setText(vo.getType());
 		
-		JLabel lblNewLabel_6 = new JLabel("바디 / 스위트");
+		JLabel lblNewLabel_6 = new JLabel("바디감 / 당도");
+		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_6.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_6);
 		
 		JLabel bosw = new JLabel("New label");
 		panel_1.add(bosw);
+		bosw.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		bosw.setText("바디감: "+vo.getBody()+"   당도: "+vo.getSweet());
 		
 		
 		JLabel lblNewLabel_8 = new JLabel("제조사");
+		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_8.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_8);
 		
 		JLabel company = new JLabel("New label");
 		panel_1.add(company);
+		company.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		company.setText(vo.getCompany());
 		
 		JLabel lblNewLabel_13 = new JLabel("어울리는 음식");
+		lblNewLabel_13.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_13.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		panel_1.add(lblNewLabel_13);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -135,6 +156,7 @@ public class WineDetail extends JFrame implements ActionListener{
 		
 		
 		JLabel food = new JLabel("New label");
+		food.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		scrollPane.setViewportView(food);
 		//panel_1.add(food);
 		food.setText(vo.getFood());
